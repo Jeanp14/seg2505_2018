38fa9f5b16fb2df0c3d15413eba3af03c67e0ac6
karljr11 <kdupu059@uottawa.ca>
2018-09-11 18:45:32 -0400

    ajouter message

--- labs/01_git/PersonInfo4.java
+++ labs/01_git/PersonInfo4.java
@@ -1,9 +1,9 @@
 public class PersonInfo4 {
 
-    public static String firstName = "Alex";
-    public static String lastName = "Moujot";
-    public static int studentNo = 88499;
-    public static String description = "les ananas";
+    public static String firstName = "Karl";
+    public static String lastName = "Dupuy";
+    public static int studentNo = 300018154;
+    public static String description = "Messi le GOAT";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
