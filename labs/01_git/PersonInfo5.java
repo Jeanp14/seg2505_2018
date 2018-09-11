a1eef8dc4764f367e79605ff29e68c4314cdebe2
Jean-Pamphile Goutondji <jgout073@uOttawa.ca>
2018-09-11 18:12:00 -0400

    change info

--- labs/01_git/PersonInfo5.java
+++ labs/01_git/PersonInfo5.java
@@ -1,9 +1,9 @@
 public class PersonInfo5 {
 
-    public static String firstName = "Billy";
-    public static String lastName = "Jean";
-    public static int studentNo = 44949382;
-    public static String description = "Not the best lover";
+    public static String firstName = "Jean-Pamphile";
+    public static String lastName = "Goutondji";
+    public static int studentNo = 300019368;
+    public static String description = "Hi";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
